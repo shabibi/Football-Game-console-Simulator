158e2613c2b63abfbbeed17baac78bd3dfc69920
Afra Alshabibi <a.shabibi20@gmail.com>
2024-10-09 11:36:11 +0400

    Create Player class ,Adding it's Attributes and Constructor

---
+++ Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotballGame
+{
+    public abstract class Player
+    {
+        // Attributes:
+        public string playerName;
+        public int skillLevel;
+        public enum Positions { Forward, Midfielder, Defender, Goalkeeper };
+        public Positions position;
+
+        //Constructor
+        public Player(string playerName, int skillLevel, Positions position)
+        {
+            this.playerName = playerName;
+            this.skillLevel = skillLevel;
+            this.position = position;
+        }
+    }
+}
